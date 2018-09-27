1cfad6a1112c21d04551fce8b1e855e51dcaad7b
MrZWH <1875290692@qq.com>
2018-09-28 01:14:38 +0800

    44-8 canvas运动渲染不断移动的地板

--- Main.js
+++ Main.js
@@ -1,6 +1,7 @@
 // 初始化整个游戏的精灵，作为游戏开始的入口
 import {ResourceLoader} from './js/base/ResourceLoader.js';
 import {BackGround} from './js/runtime/BackGround.js';
+import {Land} from './js/runtime/Land.js';
 import {DataStore} from './js/base/DataStore.js';
 import {Director} from './js/Director.js';
 
@@ -22,7 +23,8 @@ export class Main {
 
 	init() {
 		this.dataStore
-			.put('background', BackGround);
+			.put('background', BackGround)
+			.put('land', Land);
 		Director.getInstance().run();
 	}
 }
--- js/runtime/Land.js
+++ js/runtime/Land.js
@@ -1,4 +1,31 @@
-// 不断移动的陆地
-export class Land {
+import {Sprite} from '../base/Sprite.js';
 
+// 不断移动的陆地
+export class Land extends Sprite {
+	constructor() {
+		const image = Sprite.getImage('land');
+		super(image, 0, 0,
+			image.width, image.height,
+			0, window.innerHeight - image.height,
+			image.width, image.height);
+
+		this.landX = 0;
+		this.landSpeed = 2;
+	}
+
+	draw() {
+		this.landX = this.landX + this.landSpeed;
+		if(this.landX > (this.img.width - window.innerWidth)) {
+			this.landX = 0;
+		}
+		super.draw(this.img,
+			this.srcX,
+			this.srcY,
+			this.srcW,
+			this.srcH,
+			-this.landX,
+			this.y,
+			this.width,
+			this.height)
+	}
 }
