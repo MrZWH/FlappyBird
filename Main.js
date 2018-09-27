4ee500031d6957208ac654764fb249378db68ed4
MrZWH <1875290692@qq.com>
2018-09-27 17:39:11 +0800

    导演类的创建

--- Main.js
+++ Main.js
@@ -1,5 +1,6 @@
 // 初始化整个游戏的精灵，作为游戏开始的入口
 import {ResourceLoader} from './js/base/ResourceLoader.js';
+import {Director} from './js/Director.js';
 
 export class Main {
 	constructor() {
--- js/Director.js
+++ js/Director.js
@@ -1,4 +1,14 @@
 // 导演类，控制游戏的逻辑
 export class Director {
+	constructor() {
+		console.log('构造器初始化')
+	}
 
+	static getInstance() {
+		if (!Director.instance) {
+			Director.instance = new Director()
+		}
+
+		return Director.instance
+	}
 }
